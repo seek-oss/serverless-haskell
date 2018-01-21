c65c3b667f24c85ec4f4dd7201375d3089c2c6fe
Alexey Kotlyarov <a@koterpillar.com>
2018-01-21 19:51:49 +1100

    Integration test: verify passing arguments to the executable

--- integration-test/Main.hs
+++ integration-test/Main.hs
@@ -1,12 +1,22 @@
+{-|
+Main module for the integration test.
+-}
 import qualified Data.Aeson as Aeson
 
 import AWSLambda
+
+import System.Environment
 
 main :: IO ()
 main = lambdaMain handler
 
 handler :: Aeson.Value -> IO [Int]
 handler evt = do
+  -- Test logs going through
   putStrLn "This should go to logs"
+  -- Test passed arguments
+  getArgs >>= print
+  -- Test passed event
   print evt
+  -- Test return value
   pure [1, 2, 3]
