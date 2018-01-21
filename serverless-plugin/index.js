fcbff50da9d9761f4ccb146332efb11bfe29a816
Alexey Kotlyarov <a@koterpillar.com>
2018-01-22 09:38:59 +1100

    Better explanation for disabling Node dependencies filtering

--- serverless-plugin/index.js
+++ serverless-plugin/index.js
@@ -49,7 +49,11 @@ class ServerlessPlugin {
             haveImage: false,
         };
 
-        // Do not try to look into Node dependencies
+        // By default, Serverless examines node_modules to figure out which
+        // packages there are from dependencies versus devDependencies of a
+        // package. While there will always be a node_modules due to Serverless
+        // and this plugin being installed, it will be excluded anyway.
+        // Therefore, the filtering can be disabled to speed up the process.
         this.serverless.service.package.excludeDevDependencies = false;
 
         this.additionalFiles = [];
