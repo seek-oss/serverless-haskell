551a3a9c175fd4f2d353d63429306474a4ee421f
Alexey Kotlyarov <a@koterpillar.com>
2018-01-21 20:04:02 +1100

    Integration test: verify including extra libraries

--- integration-test/Main.hs
+++ integration-test/Main.hs
@@ -1,11 +1,15 @@
 {-|
 Main module for the integration test.
 -}
+{-# LANGUAGE OverloadedStrings #-}
+
 import qualified Data.Aeson as Aeson
 
 import AWSLambda
 
 import System.Environment
+
+import Text.Regex.PCRE.Light
 
 main :: IO ()
 main = lambdaMain handler
@@ -16,6 +20,8 @@ handler evt = do
   putStrLn "This should go to logs"
   -- Test passed arguments
   getArgs >>= print
+  -- Test working with an included extra library (libpcre)
+  print $ match (compile "[a-z]+" []) "012abc345" []
   -- Test passed event
   print evt
   -- Test return value
