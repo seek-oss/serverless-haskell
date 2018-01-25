94ac5803da4840be4017b92c1d326af28e1d0bd3
David Overton <doverton@seek.com.au>
2018-01-25 23:10:11 +1100

    Move Embedded to a more sensible location

--- test/AWSLambda/Events/SNSEventSpec.hs
+++ test/AWSLambda/Events/SNSEventSpec.hs
@@ -3,10 +3,10 @@
 
 module AWSLambda.Events.SNSEventSpec where
 
-import           AWSLambda.Events.Embedded
 import           AWSLambda.Events.Records
 import           AWSLambda.Events.S3Event
 import           AWSLambda.Events.SNSEvent
+import           Data.Aeson.Embedded
 
 import           Data.Aeson
 import           Data.ByteString.Lazy      (ByteString)
