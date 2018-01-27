ef5d2a4a3e17ada78bdb2feafb30ab147f8d088c
David Overton <doverton@seek.com.au>
2018-01-28 10:49:06 +1100

    Factor out testEncodeDecode into TestUtil

--- test/Data/Aeson/EmbeddedSpec.hs
+++ test/Data/Aeson/EmbeddedSpec.hs
@@ -3,16 +3,13 @@ module Data.Aeson.EmbeddedSpec where
 
 import Data.Aeson
 import Data.Aeson.Embedded
+import Data.Aeson.TestUtil
 
 import Test.Hspec
 
 spec :: Spec
 spec = describe "Embedded" $ do
-  let test d b v = describe d $ do
-        it "decodes" $
-          decode b `shouldBe` Just (Embedded v)
-        it "encodes" $
-          encode (Embedded v) `shouldBe` b
-  test "array" "\"[1,2,3]\"" [1::Int, 2, 3]
-  test "string" "\"\\\"foo\\\"\"" ("foo"::String)
-  test "object" "\"{\\\"a\\\":42,\\\"b\\\":true}\"" (object ["a" .= (42::Int), "b" .= True])
+  testEncodeDecode "array" "\"[1,2,3]\"" $ Embedded [1::Int, 2, 3]
+  testEncodeDecode "string" "\"\\\"foo\\\"\"" $ Embedded ("foo"::String)
+  testEncodeDecode "object" "\"{\\\"a\\\":42,\\\"b\\\":true}\"" $ Embedded $ object ["a" .= (42::Int), "b" .= True]
+  testEncodeDecode "in field" "{\"a\":\"[1,2,3]\"}" $ object ["a" .= Embedded [1::Int, 2, 3]]
