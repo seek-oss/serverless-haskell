947ba31464c882019fb32018c7713fc367a1a667
Alexey Kotlyarov <a@koterpillar.com>
2020-12-13 16:16:52 +1100

    Use AWS official base image

--- config.ts
+++ config.ts
@@ -9,7 +9,9 @@ export const HASKELL_RUNTIME: Runtime = 'haskell';
 export const BASE_RUNTIME: Runtime = 'provided.al2';
 
 // Docker image used as reference
-export const DOCKER_IMAGE = 'lambci/lambda:' + BASE_RUNTIME;
+// https://aws.amazon.com/blogs/aws/new-for-aws-lambda-container-image-support/
+// https://hub.docker.com/r/amazon/aws-lambda-provided
+export const DOCKER_IMAGE = 'amazon/aws-lambda-provided:al2';
 
 // Docker image used for the builds. This is used even for building LTS 14
 // projects, because this is the last Stack image with glibc version compatible
