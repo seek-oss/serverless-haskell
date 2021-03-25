a99b5dfa31809c9ed4d1c1f406bd87499a3f3b37
Alexey Kotlyarov <a@koterpillar.com>
2021-03-25 17:09:36 +1100

    Change base Docker image

--- config.ts
+++ config.ts
@@ -13,7 +13,6 @@ export const BASE_RUNTIME: Runtime = 'provided.al2';
 // https://hub.docker.com/r/amazon/aws-lambda-provided
 export const DOCKER_IMAGE = 'amazon/aws-lambda-provided:al2';
 
-// Docker image used for the builds. This is used even for building LTS 14
-// projects, because this is the last Stack image with glibc version compatible
-// to the one on the AWS Lambda environment.
-export const BUILD_DOCKER_IMAGE = 'fpco/stack-build:lts-13.30';
+// Docker image used for the builds. This needs to have all dependencies for
+// running GHC and Stack, but can't have glibc later what AWS provides.
+export const BUILD_DOCKER_IMAGE = 'haskell:stretch';
