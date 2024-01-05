89c733988e229ceb7c0ddfc45b46f44125d67569
Jan Hrček <honza.hrk@gmail.com>
2024-01-05 12:16:05 +0100

    Use al2023 runtime image by default

--- config.ts
+++ config.ts
@@ -6,12 +6,12 @@ export type Runtime = string;
 export const HASKELL_RUNTIME: Runtime = 'haskell';
 
 // Runtime used by the wrapper
-export const BASE_RUNTIME: Runtime = 'provided.al2';
+export const BASE_RUNTIME: Runtime = 'provided.al2023';
 
 // Docker image used as reference
 // https://aws.amazon.com/blogs/aws/new-for-aws-lambda-container-image-support/
 // https://hub.docker.com/r/amazon/aws-lambda-provided
-export const DOCKER_IMAGE = 'amazon/aws-lambda-provided:al2';
+export const DOCKER_IMAGE = 'amazon/aws-lambda-provided:al2023';
 
 // Docker image used for the builds. This needs to have all dependencies for
 // running GHC and Stack, but can't have glibc later what AWS provides.
